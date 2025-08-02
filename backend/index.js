d0c076db51a45879d72b3ce7d9732f05c93c28a4
Ritika Srivastava <ritika.srivastava315@gmail.com>
2025-08-02 10:04:33 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,7 @@ require('dotenv').config()
 // middleware
 app.use(express.json());
 app.use(cors({
-    origin: ['http://localhost:5173', 'http://book-store-eight-lac.vercel.app/'],
+    origin: ['http://localhost:5173', 'http://book-store-eight-lac.vercel.app'],
     credentials: true
 }))
 
